3c2f8b050870ab1a8773026ec630553107750b0b
jianheng <2276145687@qq.com>
2024-07-22 15:04:02 +0100

    Create Topomap

--- visualnav-transformer/deployment/src/my_create_topomap.sh
+++ visualnav-transformer/deployment/src/my_create_topomap.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# 1st Parameter: topo map name
+topo_map_name="${1:-warehouse_turtlebot}"
+# 2nd Parameter: bag file name (with .bag) in the topomaps/bags directory
+bag_name="${2:-warehouse_turtlebot.bag}"
+# 3rd Parameter: delay between messages in the rosbag play command
+delay="${3:-1.5}"
+
 # Create a new tmux session
-session_name="gnm_locobot_$(date +%s)"
+session_name="GNM_Create_Topomap"
 tmux new-session -d -s $session_name
 
 # Split the window into three panes
@@ -17,13 +24,14 @@ tmux send-keys "roscore" Enter
 # Run the create_topoplan.py script with command line args in the second pane
 tmux select-pane -t 1
 tmux send-keys "conda activate vint_deployment" Enter
-tmux send-keys "python create_topomap.py --dt 1 --dir $1" Enter
+tmux send-keys "python create_topomap.py --dt 1 --dir $topo_map_name" Enter
 
 # Change the directory to ../topomaps/bags and run the rosbag play command in the third pane
 tmux select-pane -t 2
-tmux send-keys "mkdir -p ../topomaps/bags" Enter
 tmux send-keys "cd ../topomaps/bags" Enter
-tmux send-keys "rosbag play -r 1.5 $2" # feel free to change the playback rate to change the edge length in the graph
+tmux send-keys "rosbag play -r $delay $bag_name" Enter # feel free to change the playback rate to change the edge length in the graph
+
+tmux selectp -t 2
 
 # Attach to the tmux session
 tmux -2 attach-session -t $session_name
